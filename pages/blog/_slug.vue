767ceaf7e6e1f17ad2ddea2a725abaf318f8ec26
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-06-20 12:31:44 -0700

    Remove some code.

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -2,19 +2,30 @@
 <template>
 	<article>
 		<h1>{{ title }}</h1>
+
+		<small>{{ date }}</small>
+
+		<p>{{ body }}</p>
 	</article>
 </template>
 
 <script>
+
 	export default {
+
 		async asyncData({ params }) {
+
 			// const postPromise = process.BROWSER_BUILD
 			//   ? import('~/content/blog/posts/' + params.slug + '.json')
 			//   : Promise.resolve(
 			//       require('~/content/blog/posts/' + params.slug + '.json')
 			//     );
-			let post = await import('~/content/blog/posts/' + params.slug + '.json');
+			
+			let post = await import( '~/content/blog/posts/' + params.slug + '.json' );
 			return post;
+
 		}
+
 	};
+
 </script>
