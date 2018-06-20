0cac147b0124a23176352314d307fe96d154cb6e
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-06-20 12:16:58 -0700

    Agregar la ruta de los posts.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,15 @@
+
+var glob = require('glob');
+var path = require('path');
+
+// Enhance Nuxt's generate process by gathering all content files from Netifly CMS
+// automatically and match it to the path of your Nuxt routes.
+// The Nuxt routes are generate by Nuxt automatically based on the pages folder.
+
+var dynamicRoutes = getDynamicPaths({
+  '/blog': 'blog/posts/*.json'
+});
+
 module.exports = {
   /*
   ** Headers of the page
@@ -35,4 +47,20 @@ module.exports = {
       }
     }
   }
+}
+
+/**
+ * Create an array of URLs from a list of files
+ * @param {*} urlFilepathTable
+ */
+
+function getDynamicPaths(urlFilepathTable) {
+  return [].concat(
+    ...Object.keys(urlFilepathTable).map(url => {
+      var filepathGlob = urlFilepathTable[url];
+      return glob
+        .sync(filepathGlob, { cwd: 'content' })
+        .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
+    })
+  );
 }
