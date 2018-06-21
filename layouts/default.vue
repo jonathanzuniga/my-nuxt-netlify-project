30525ddd7d348c41b6f2302f58345937b24b9599
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-06-21 10:10:09 -0700

    Add a contact page with a netlify form.

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,9 @@
 				<li>
 					<nuxt-link to="/about">About</nuxt-link>
 				</li>
+				<li>
+					<nuxt-link to="/contact">Contact</nuxt-link>
+				</li>
 			</ul>
 		</nav>
 
---
+++ pages/contact.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<h1>
+			Contact
+		</h1>
+
+		<div>
+			<form name="contact" action="" method="post" netlify>
+
+				<input type="hidden" name="form-name" value="contact">
+
+				<p>
+					<label for="name">
+						Name:
+					</label>
+					<input name="name" id="name">
+				</p>
+
+				<p>
+					<label for="email">
+						Email:
+					</label>
+					<input name="email" id="email">
+				</p>
+
+				<p>
+					<label for="message">
+						Message:
+					</label>
+					<textarea name="message" id="message"></textarea>
+				</p>
+
+				<input type="submit" value="Send message">
+
+			</form>
+		</div>
+	</div>
+</template>
