e7d1a6a81e3c5b7d6cf6ee7cd28acc25a3a3fdc3
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-06-21 10:22:43 -0700

    Improve the contact form and add a thank you page.

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -1,9 +1,12 @@
-<div>
-	<h1 class="title">
-		Thank you
-	</h1>
 
-	<p>
-		<nuxt-link to="/">Back to home</nuxt-link>
-	</p>
-</div>
+<template>
+	<div>
+		<h1 class="title">
+			Thank you
+		</h1>
+
+		<p>
+			<nuxt-link to="/">Back to home</nuxt-link>
+		</p>
+	</div>
+</template>
